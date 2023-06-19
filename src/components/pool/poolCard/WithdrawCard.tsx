f68a64fe6e02744762447b0fcd30c965a80ae8cc
0xXiaoChen <1255552127@qq.com>
2023-06-19 09:10:29 +0800

    fix:withdraw cord w error

--- src/components/pool/poolCard/WithdrawCard.tsx
+++ src/components/pool/poolCard/WithdrawCard.tsx
@@ -7,7 +7,7 @@ const WithdrawCard = () => {
     <div>
       {/* 写一个swap卡片样式,上下左右都居中 */}
       <div className="flex justify-center items-center">
-        <div className="   bg-white  bg-opacity-30 rounded-xl shadow-xl flex-col p-4 w-[480px]">
+        <div className="   bg-white  bg-opacity-30 rounded-xl shadow-xl flex-col p-4">
           <WithdrawCard_Header />
           <WithdrawCard_Content />
         </div>
