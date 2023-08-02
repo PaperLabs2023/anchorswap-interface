d425c3a70f2955bf9e181ee7f3c38d1416424b40
Wanten <WantenMN@gmail.com>
2023-08-02 09:58:28 +0800

    chore: setup path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: 'globalThis',
+    global: "globalThis",
   },
   resolve: {
     alias: {
-      process: 'process/browser',
-      util: 'util',
+      process: "process/browser",
+      util: "util",
     },
   },
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+});
