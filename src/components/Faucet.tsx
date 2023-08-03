51c8c4a482f45ab4bdd6b08d46187deceff527aa
Wanten <WantenMN@gmail.com>
2023-08-03 19:26:23 +0800

    refactor(Faucet): clean up and resolve issues

--- src/components/Faucet.tsx
+++ src/components/Faucet.tsx
@@ -1,25 +1,19 @@
-//@xiaochen
 import { useState } from "react";
 import {
   useAccount,
-  useContractReads,
   usePrepareContractWrite,
   useContractWrite,
   useWaitForTransaction,
+  useContractRead,
 } from "wagmi";
-
-import { ethers } from "ethers";
-import { tPaper, tFaucet, oPaper } from "@/contracts";
+import { tPaper, tFaucet } from "@/contracts";
 
 export default function Faucet() {
   const { address } = useAccount();
   const [isFaucted_A, setIsFaucted_A] = useState(false);
-  const [isFaucted_B, setIsFaucted_B] = useState(false);
   const [isLoading_A, setIsLoading_A] = useState(false);
-  const [isLoading_B, setIsLoading_B] = useState(false);
-  const [tokenA_balance, setTokenABalance] = useState("0");
-  const [tokenB_balance, setTokenBBalance] = useState("0");
   const [hash, setHash] = useState<`0x${string}`>("0x");
+
   const faucetClick_A = () => {
     if (isFaucted_A) {
       alert("You have already got tokenA");
@@ -31,6 +25,7 @@ export default function Faucet() {
           setHash(res.hash);
         })
         .catch((err) => {
+          console.error(err);
           setIsLoading_A(false);
         });
     }
@@ -42,88 +37,29 @@ export default function Faucet() {
     enabled: hash !== "0x",
     onSuccess() {
       setIsLoading_A(false);
-      setIsLoading_B(false);
     },
   });
 
-  const faucetTokenA_Config = {
+  useContractRead({
     address: tPaper.address,
     abi: tPaper.abi,
-  };
-
-  const faucetTokenB_Config = {
-    address: oPaper.address,
-    abi: tPaper.abi,
-  };
-  // 获取两种测试币的状态
-  useContractReads({
-    contracts: [
-      {
-        ...faucetTokenA_Config,
-        functionName: "faucetedList",
-        args: [address],
-      },
-      {
-        ...faucetTokenB_Config,
-        functionName: "faucetedList",
-        args: [address],
-      },
-      {
-        ...faucetTokenA_Config,
-        functionName: "balanceOf",
-        args: [address],
-      },
-      {
-        ...faucetTokenB_Config,
-        functionName: "balanceOf",
-        args: [address],
-      },
-    ],
+    functionName: "balanceOf",
+    args: [address ? address : "0x0"],
+    enabled: !!address,
     watch: true,
-    enabled: true,
-    onSuccess(data: any) {
-      setIsFaucted_A(data[0]);
-      setIsFaucted_B(data[1]);
-      setTokenABalance(
-        Number(ethers.utils.formatUnits(data[2].result, "ether"))
-          .toFixed(6)
-          .replace(/\.?0+$/, "")
-      );
-
-      setTokenBBalance(
-        Number(ethers.utils.formatUnits(data[3].result, "ether"))
-          .toFixed(6)
-          .replace(/\.?0+$/, "")
-      );
+    onSuccess(data) {
+      setIsFaucted_A(!!data);
     },
   });
 
-  // Faucet config
   const { config: faucetAConfig } = usePrepareContractWrite({
     address: tFaucet.address,
     abi: tFaucet.abi,
     functionName: "claim",
   });
-  // Faucet
-  const { data: faucetConfigData, writeAsync: faucetAConfigWrite } =
-    useContractWrite({
-      ...faucetAConfig,
-      onError(error) {
-        console.log("Error", error);
-      },
-    });
 
-  // Faucet config
-  const { config: faucetBConfig } = usePrepareContractWrite({
-    address: oPaper.address,
-    abi: tPaper.abi,
-    functionName: "faucet",
-    args: [],
-    // account: address,
-  });
-  // Faucet
-  useContractWrite({
-    ...faucetBConfig,
+  const { writeAsync: faucetAConfigWrite } = useContractWrite({
+    ...faucetAConfig,
     onError(error) {
       console.log("Error", error);
     },
