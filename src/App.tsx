eb4be855bc73d19dcba932aeb39f706039a2a1d0
Wanten <WantenMN@gmail.com>
2023-08-04 09:57:59 +0800

    refactor(App): update network switching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import { useNetwork, useSwitchNetwork } from "wagmi";
+import { useSwitchNetwork } from "wagmi";
 import { Outlet } from "react-router-dom";
-
 import AppHeader from "./layout/AppHeader/AppHeader";
-
 import { useEffect } from "react";
 import NavBar from "./layout/NavBar";
+import { zkSyncTestnet } from "wagmi/chains";
 
 const App = () => {
-  const { chain } = useNetwork();
   const { switchNetwork } = useSwitchNetwork({
-    chainId: 280,
+    chainId: zkSyncTestnet.id,
   });
 
   useEffect(() => {
-    if (chain?.id !== 280 && switchNetwork) {
+    if (switchNetwork) {
       switchNetwork();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [switchNetwork]);
 
   return (
